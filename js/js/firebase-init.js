9636a60b7e9e12ba0a22101ad9bc91e4f5e86a1c
himaghimire113-cell <himaghimire113@gmail.com>
2026-07-18 11:55:44 +0545

    Create firebase-init.js

---
+++ js/js/firebase-init.js
@@ -0,0 +1,33 @@
+// ============================================================
+// FIREBASE CONFIGURATION
+// ============================================================
+const firebaseConfig = {
+  apiKey: "AIzaSyBVD4VIxCJAXoMbhSmsFi1VeA38RP9aErM",
+  authDomain: "mind-hackss.firebaseapp.com",
+  projectId: "mind-hackss",
+  storageBucket: "mind-hackss.firebasestorage.app",
+  messagingSenderId: "522496244281",
+  appId: "1:522496244281:web:469eaa354279db14d5ebe8"
+};
+
+// Google Analytics Measurement ID
+const GA_MEASUREMENT_ID = "G-D83LP4Y3P0";
+
+// ---- Do not edit below this line ----
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+if (GA_MEASUREMENT_ID) {
+  const gaScript = document.createElement("script");
+  gaScript.async = true;
+  gaScript.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+  document.head.appendChild(gaScript);
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  gtag("js", new Date());
+  gtag("config", GA_MEASUREMENT_ID);
+  window.gtag = gtag;
+} else {
+  window.gtag = function () {};
+}
